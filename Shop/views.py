2c3123d25e7abcc7f04a5e8727c03ea1a8c60751
ZakariyaeABBIH <75364734+zakariyae1889@users.noreply.github.com>
2023-09-02 23:23:40 +0100

    Update views.py

--- Shop/views.py
+++ Shop/views.py
@@ -1,21 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.views.generic import *
-
-class EcommerceApp():
-    #---------------------HomePage-----------------------#
+from .models import *
+#---------------------HomePage-----------------------#
+class HomeViews():
+   
     def PageHome(request):
         return render(request,template_name='Home/home.html')
-    #---------------------ShopPage-----------------------#
-    def PageShop(request):
-        return render(request,template_name='Shop/shop.html')
-    #---------------------ShopDetailsPage-----------------------#
-    def PageShopDetail(request):
-        return render(request,template_name='Shop/detail.html')
-    #---------------------CartPage-----------------------#
-    def PageCart(request):
+#---------------------ShopPage-----------------------#
+class ShopViews(ListView):
+    model=Products
+    template_name='Shop/shop.html'
+    paginate_by=6
+    context_object_name='product'
+#---------------------ShopDetailsPage-----------------------#
+class ShopDeatilsView(DetailView):
+    model=Products
+    template_name='Shop/detail.html'
+    context_object_name='product'
+#---------------------CartPage-----------------------#
+class CartViews():
+    def PageCart(request,slug):
         return render(request,template_name='Shop/cart.html')
-    #---------------------CheckoutPage-----------------------#
+#---------------------CheckoutPage-----------------------#
+class CheckoutViews():
     def PageCheckout(request):
         return render(request,template_name='Shop/checkout.html')
-     
      
