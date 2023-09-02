19572eb78a6d6a52cde99a20c3d0427810a71170
ZakariyaeABBIH <75364734+zakariyae1889@users.noreply.github.com>
2023-09-02 16:33:06 +0100

    Update views.py

--- Shop/views.py
+++ Shop/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+class EcommerceApp():
+    #---------------------HomePage-----------------------#
+    def PageHome(request):
+        return render(request,template_name='Home/home.html')
+    #---------------------ShopPage-----------------------#
+    def PageShop(request):
+        return render(request,template_name='Shop/shop.html')
+    #---------------------ShopDetailsPage-----------------------#
+    def PageShopDetail(request):
+        return render(request,template_name='Shop/detail.html')
+    #---------------------CartPage-----------------------#
+    def PageCart(request):
+        return render(request,template_name='Shop/cart.html')
+    #---------------------CheckoutPage-----------------------#
+    def PageCheckout(request):
+        return render(request,template_name='Shop/checkout.html')
+     
+     
