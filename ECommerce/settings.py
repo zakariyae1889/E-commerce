432b3a78094ef4c1b1b67097634af03f47d22400
AbbihZakariyae <abbihzakariyae@gmail.com>
2023-09-30 21:29:24 +0100

    install django-heroku and update settings

--- ECommerce/settings.py
+++ ECommerce/settings.py
@@ -1,5 +1,8 @@
+import django_heroku
+
 from pathlib import Path
 import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -47,6 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -138,12 +142,17 @@ else:
 
 MEDIA_ROOT=os.path.join(BASE_DIR,'media/')
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 
 LOGIN_REDIRECT_URL='Path_Home'
 LOGIN_URL='Path_Login'
+
+django_heroku.settings(locals())
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
